aac993ddcc67dd0d15545f4fa8ac6e613ffa6fb2
MahmoudGhannam <mahmou999@gmail.com>
2020-08-26 01:42:19 +0300

    add js file

--- js/add-content.js
+++ js/add-content.js
@@ -3,4 +3,17 @@ var yourname = prompt("what's you name plz?")
 alert("hello " + yourname + " welcome to our site");
 
 
-var r = confirm("Press ok to complete");
+function paintIt()
+{
+    color = prompt("Enter the color you want on the Background???");
+    document.body.style.backgroundColor = color;
+}
+// Function to change webpage background color
+function changeBodyBg(color){
+    document.body.style.background = color;
+}
+
+// Function to change heading background color
+function changeHeadingBg(color){
+    document.getElementById("heading").style.background = color;
+}
