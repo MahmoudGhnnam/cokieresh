94eab48511a62e866f62f8a14ffc000bc2e7153a
MahmoudGhannam <mahmou999@gmail.com>
2020-08-26 02:31:29 +0300

    add js file

--- js/add-content.js
+++ js/add-content.js
@@ -3,3 +3,5 @@ var yourname = prompt("what's you name plz?")
 alert("hello " + yourname + " welcome to our site");
 
 var r = confirm("Press ok");
+
+alert("clik on clik here to pay to change color page :)");
