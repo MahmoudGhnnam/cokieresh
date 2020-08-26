ca4e471a785e3ba156364f8a098a88f6a9473d96
MahmoudGhannam <mahmou999@gmail.com>
2020-08-26 17:09:03 +0300

    add while

--- js/add-content.js
+++ js/add-content.js
@@ -1,18 +1,50 @@
-function greeting(){
+function greeting() {
     var userChoice = prompt('do you have Diabetes')
     var result;//
     if (userChoice == 'yes') {
         greeting = 'you cant buy cookies!';
-      } else if (userChoice == 'no') {
+    } else if (userChoice == 'no') {
         greeting = 'you can buy cookies :)';
-      } else {
+    } else {
         greeting = 'some thing wrong!';
-      }
+    }
 
-      return greeting;
+    return greeting;
 }
+document.write('<h3>' + greeting() + '</h3>');
 
-document.write('<h3>'+greeting()+'</h3>');
+function showOrder () {
+    var userInput = prompt('what would you like to order nutella cookies or classic cookies or lotus cookies');
+    while (userInput !== "nutella cookies" && userInput !== "classic cookies" && userInput !== "lotus cookies") {
+        userInput = prompt('you should choose from thi three:) nutella cookies or classic cookies or lotus cookies');
+    }
+   
+    var userOrder = "";
+    var numberOfImage = prompt("how many cookies do you want to buy");
+    
+    for (var count = 0; count < numberOfImage; count++) {
+        if (userInput == "nutella cookies") {
+            
+            userOrder = userOrder + "<img src='img/nutella.jpg' alt=''>";
+            console.log(userOrder);
+        }
+        else if (userInput == "classic cookies") {
+           
+            userOrder = userOrder + "<img src='img/classic.jpg' alt=''>";
+            console.log(userOrder);
+        }
+        else if (userInput == "lotus cookies") {
+            
+            userOrder = userOrder + "<img src='img/lotus.jpg' alt=''>";
+            console.log(userOrder);
+        }
+    }
+    //output
+    return userOrder;
+}
+document.write('<h3>' + showOrder() + '</h3>');
+
+
 
 var yourname = prompt("what's you name plz?")
 
@@ -22,5 +54,5 @@ var r = confirm("Press ok");
 
 function changeBg() {
     newCol = prompt("What colour would you like the page's background to be?");
-    document.body.style.background=newCol;
-    }
+    document.body.style.background = newCol;
+}
